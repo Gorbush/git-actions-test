9ca92c2624aae066c7c78ec6334ef0df5ca5b0c8
Sergii Puliaiev <spuliaiev@salesforce.com>
2021-09-03 14:13:42 -0700

    Hello World controller.

---
+++ src/main/java/com/home/marina/gitactionstest/GitActionsTestApplication.java
@@ -0,0 +1,9 @@
+package com.home.marina.gitactionstest;
+
+public class GitActionsTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, Git Actions!");
+	}
+
+}
---
+++ src/test/java/com/home/marina/gitactionstest/GitActionsTestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.home.marina.gitactionstest;
+
+import org.junit.jupiter.api.Test;
+
+
+class GitActionsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
